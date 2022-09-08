cf7123f60d4fcd861710923b5ac87953731e08ca
Henrique Neto <henriqueneto1912@gmail.com>
2022-09-08 10:18:12 -0300

    Adicionando telas

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -7,27 +7,33 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
     private static Stage stage;
     private static Scene channelConfigScreenToken;
     private static Scene channelConfigScreenUserPass;
+    private static Scene activeConfigScreen;
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) {
         // TODO: Código principal inserido aqui dentro
         stage = primaryStage;
+        primaryStage.setResizable(false);
         try {
-            AnchorPane fxmlChannelConfigScreenToken = FXMLLoader.load(getClass().getResource("/fxmlConfigCanaisToken.fxml"));
-            AnchorPane fxmlchannelConfigScreenUserPass = FXMLLoader.load(getClass().getResource("/fxmlConfigCanaisUsuarioESenha.fxml"));
+            AnchorPane fxmlChannelConfigScreenToken = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigCanaisToken.fxml")));
             channelConfigScreenToken = new Scene(fxmlChannelConfigScreenToken);
-            channelConfigScreenUserPass = new Scene(fxmlchannelConfigScreenUserPass);
+
+            AnchorPane fxmlChannelConfigScreenUserPass = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigCanaisUsuarioESenha.fxml")));
+            channelConfigScreenUserPass = new Scene(fxmlChannelConfigScreenUserPass);
+
+            AnchorPane fxmlActiveConfigScreen = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigAtiva.fxml")));
+            activeConfigScreen = new Scene(fxmlActiveConfigScreen);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // primaryStage.setScene(channelConfigScreenToken);
-        primaryStage.setScene(channelConfigScreenUserPass);
+        primaryStage.setScene(activeConfigScreen);
         primaryStage.show();
     }
 
