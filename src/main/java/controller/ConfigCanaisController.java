b4b76dbef26cfb36061b217382c247f49fcd8a42
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-09-08 09:59:19 -0300

    feat: :sparkles: Adicionadas as telas de Configuração de Canais e modificado arquivo Main

---
+++ src/main/java/controller/ConfigCanaisController.java
@@ -0,0 +1,8 @@
+package controller;
+
+// import javafx.fxml.FXML;
+// import javafx.scene.control.Label;
+
+public class ConfigCanaisController {
+
+}
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,12 +1,34 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private static Stage stage;
+    private static Scene channelConfigScreenToken;
+    private static Scene channelConfigScreenUserPass;
+
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) throws IOException {
         // TODO: Código principal inserido aqui dentro
+        stage = primaryStage;
+        try {
+            AnchorPane fxmlChannelConfigScreenToken = FXMLLoader.load(getClass().getResource("/fxmlConfigCanaisToken.fxml"));
+            AnchorPane fxmlchannelConfigScreenUserPass = FXMLLoader.load(getClass().getResource("/fxmlConfigCanaisUsuarioESenha.fxml"));
+            channelConfigScreenToken = new Scene(fxmlChannelConfigScreenToken);
+            channelConfigScreenUserPass = new Scene(fxmlchannelConfigScreenUserPass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // primaryStage.setScene(channelConfigScreenToken);
+        primaryStage.setScene(channelConfigScreenUserPass);
+        primaryStage.show();
     }
 
     public static void main(String[] args) { launch(args); }
