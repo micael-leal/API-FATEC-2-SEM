eba523f898ecf6d275ec7586f549ef63532faf79
Henrique Neto <henriqueneto1912@gmail.com>
2022-09-15 07:23:11 -0300

    :boom: Arrumando merge

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -6,40 +6,47 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class Main extends Application {
 
     private static Stage stage;
-    private static Scene channelConfigScreenToken;
-    private static Scene channelConfigScreenUserPass;
+    private static Scene channelConfigTokenScreen;
+    private static Scene channelConfigLoginScreen;
     private static Scene activeConfigScreen;
-    private static Scene cadastroCanais;
+    private static Scene channelRegisterScreen;
 
     @Override
     public void start(Stage primaryStage) {
-        // TODO: Código principal inserido aqui dentro
         stage = primaryStage;
         primaryStage.setResizable(false);
+
         try {
             AnchorPane fxmlChannelConfigScreenToken = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigCanaisToken.fxml")));
-            channelConfigScreenToken = new Scene(fxmlChannelConfigScreenToken);
+            channelConfigTokenScreen = new Scene(fxmlChannelConfigScreenToken);
 
             AnchorPane fxmlChannelConfigScreenUserPass = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigCanaisUsuarioESenha.fxml")));
-            channelConfigScreenUserPass = new Scene(fxmlChannelConfigScreenUserPass);
+            channelConfigLoginScreen = new Scene(fxmlChannelConfigScreenUserPass);
 
             AnchorPane fxmlActiveConfigScreen = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlConfigAtiva.fxml")));
             activeConfigScreen = new Scene(fxmlActiveConfigScreen);
 
             AnchorPane fxmlChannelRegister = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlCadastroCanais.fxml")));
-            cadastroCanais = new Scene(fxmlChannelRegister);
-
+            channelRegisterScreen = new Scene(fxmlChannelRegister);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        primaryStage.setScene(cadastroCanais);
+        primaryStage.setScene(activeConfigScreen);
         primaryStage.show();
+    }
+
+    public static void changeScene(String scene) {
+        switch (scene) {
+            case "ConfigToken" -> stage.setScene(channelConfigTokenScreen);
+            case "ConfigLogin" -> stage.setScene(channelConfigLoginScreen);
+            case "ActiveConfig" -> stage.setScene(activeConfigScreen);
+            case "ChannelRegister" -> stage.setScene(channelRegisterScreen);
+        }
     }
 
     public static void main(String[] args) { launch(args); }
