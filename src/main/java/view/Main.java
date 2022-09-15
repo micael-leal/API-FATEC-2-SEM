fbd6cd81b26bcf52131219bd2ec18bf1ca327108
Henrique Neto <henriqueneto1912@gmail.com>
2022-09-15 12:07:48 -0300

    :construct: CRUD da tela de configurações ativas

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import model.Channel;
-import model.GetChannelById;
 
 import java.util.Objects;
 
