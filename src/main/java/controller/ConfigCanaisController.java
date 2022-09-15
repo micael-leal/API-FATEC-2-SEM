221a2818ed15faad1333f8de689d1debec695590
CamilaRedondo <caredondo97@gmail.com>
2022-09-15 08:30:49 -0300

    feat: Add CadastroCanais Padrões service

--- src/main/java/controller/ConfigCanaisController.java
+++ src/main/java/controller/ConfigCanaisController.java
@@ -3,6 +3,13 @@ package controller;
 // import javafx.fxml.FXML;
 // import javafx.scene.control.Label;
 
-public class ConfigCanaisController {
+import javafx.fxml.Initializable;
 
+public class ConfigCanaisController {
+   /* @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        comboBox.getItems().removeAll(comboBox.getItems());
+        comboBox.getItems().addAll("Option A", "Option B", "Option C");
+        comboBox.getSelectionModel().select("Option B");
+    }*/
 }
