c746e1d35edaadc93e76a6fdd3c66ce989054b47
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-09-15 11:33:59 -0300

    Adição da lógica do controller da tela de configuração de canais

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
             e.printStackTrace();
         }
 
-        primaryStage.setScene(activeConfigScreen);
+        primaryStage.setScene(channelRegisterScreen);
         primaryStage.show();
     }
 
